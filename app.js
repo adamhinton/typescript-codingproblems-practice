0dcfd44d03b03a59b334c9ffe12b602834ac7fd0
adamhinton <adam.d.hinton@gmail.com>
2022-06-01 19:47:45 -0400

    comment problem text

--- app.js
+++ app.js
@@ -11,10 +11,16 @@
 // for each, add i to solution
 // return solution;
 // ret array of all ints bw parameters exclusive
-const between = (a, b) => {
-    const solution = [];
-    for (let i = a; i <= b; i++) {
-        solution.push(i);
-    }
-    return solution;
-};
+// const between = (a: number, b: number): number[] => {
+//   const solution: number[] = [];
+//   for (let i = a; i <= b; i++) {
+//     solution.push(i);
+//   }
+//   return solution;
+// };
+// *************************************************************************************
+// Terminal game move function
+// In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+// Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+// Example:
+// move(3, 6) should equal 15
