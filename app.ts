06558bf04be5ca8392dddaa4e8b6b8dc61d33b38
adamhinton <adam.d.hinton@gmail.com>
2022-06-03 19:58:57 -0400

    small adjustment to paln

--- app.ts
+++ app.ts
@@ -103,9 +103,10 @@
 // The array will always have at least 2 elements1 and all elements will be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!
 
 // take in array
-// loop over array
+// loop over array starting at index 1
 // for each: if arr[i] - 1 !== arr [i-1] return arr[i]
 
 const firstNonConsecutive = (arr: number[]): null | number => {
-  return 1;
+  //   return 1;
+  for (let i = 1; i < arr.length; i++) {}
 };
