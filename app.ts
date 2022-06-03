61b05a9bd11631fb9367e5501448da0682d183bc
adamhinton <adam.d.hinton@gmail.com>
2022-06-02 21:13:50 -0400

    problem text

--- app.ts
+++ app.ts
@@ -61,3 +61,12 @@
 
 //   return Math.floor(hol / savingsPerBottle);
 // };
+
+// *************************************************************************************
+
+// Convert number to reversed array of digits
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+// Example(Input => Output):
+// 348597 => [7,9,5,8,4,3]
+// 0 => [0]
