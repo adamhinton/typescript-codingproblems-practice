1d87beb6c0b791dfef78a0ff7bc0ebfedabf23dc
adamhinton <adam.d.hinton@gmail.com>
2022-06-03 20:05:48 -0400

    comment prompt

--- app.js
+++ app.js
@@ -81,3 +81,5 @@
 //   }
 //   return null;
 // };
+// *************************************************************************************
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
