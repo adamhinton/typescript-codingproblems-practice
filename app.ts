791d3a1b695324d7a68714e37ee5675a1bc1f691
adamhinton <adam.d.hinton@gmail.com>
2022-06-05 22:15:17 -0400

    this should work

--- app.ts
+++ app.ts
@@ -171,6 +171,6 @@
 // Grasshopper - Function syntax debugging
 // A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
 
-function main [verb: string, noun: string]: string
-  return verb + noun
+function main(verb: string, noun: string): string {
+  return verb + noun;
 }
