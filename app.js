97b7f8e1177cfc2c44e788ba5c80c9fa67b53b92
adamhinton <adam.d.hinton@gmail.com>
2022-06-05 22:14:54 -0400

    solution skeleton

--- app.js
+++ app.js
@@ -118,3 +118,7 @@
 // *************************************************************************************
 // Grasshopper - Function syntax debugging
 // A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+function main() { }
+[verb, string, noun, string];
+string;
+return verb + noun;
