e8283734d9b6aec752ebb2da9608bf3b6795bc34
adamhinton <adam.d.hinton@gmail.com>
2022-06-05 22:14:34 -0400

    next problem text

--- app.ts
+++ app.ts
@@ -167,3 +167,5 @@
 //   string === string.toUpperCase();
 
 // *************************************************************************************
+// Grasshopper - Function syntax debugging
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
