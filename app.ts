4f2b0e2f7e8588a36314b9a140d607c0fcea6df4
adamhinton <adam.d.hinton@gmail.com>
2022-06-05 22:09:19 -0400

    plan

--- app.ts
+++ app.ts
@@ -158,3 +158,7 @@
 // "ACSKLDFJSgSKLDFJSKLDFJ" -> False
 // "ACSKLDFJSGSKLDFJSKLDFJ" -> True
 // In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+
+// PLAN:
+// Take in string
+// return string === string.toUpperCase()
