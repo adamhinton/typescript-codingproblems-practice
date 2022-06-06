e9d2f287e5663fea48da55da5284c907575ecdce
adamhinton <adam.d.hinton@gmail.com>
2022-06-05 22:15:42 -0400

    plan

--- app.js
+++ app.js
@@ -118,6 +118,9 @@
 // *************************************************************************************
 // Grasshopper - Function syntax debugging
 // A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+// PLAN:
+// rpl brackets w pren
+// open curly at start of fxn
 function main(verb, noun) {
     return verb + noun;
 }
