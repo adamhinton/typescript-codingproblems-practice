69c3638dead9d0a615cbe7d007e92a22cb94dcde
adamhinton <adam.d.hinton@gmail.com>
2022-08-14 18:40:18 -0400

    code cleanup

--- app.js
+++ app.js
@@ -122,6 +122,7 @@
 // rpl brackets w pren
 // open curly at start of fxn
 // const main = (verb: string, noun: string): string => verb + noun;
+// ==============================================
 //A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
 // Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
 // PLAN:
@@ -131,10 +132,6 @@
 // return set.length === 26s
 const isPangram = (phrase) => {
     phrase = phrase.replace(/[^a-zA-Z]/gi, "");
-    console.log("phrase:", phrase);
     const phraseSet = new Set(phrase.toLowerCase());
-    console.log("phraseSet:", phraseSet);
     return phraseSet.size === 26;
 };
-console.log('isPangram("The quick brown fox jumps over the lazy dog."):', isPangram("The quick brown fox jumps over the lazy dog."));
-console.log("isPangram('blahblahblah'):", isPangram("blahblahblah"));
--- app.ts
+++ app.ts
@@ -177,6 +177,8 @@
 
 // const main = (verb: string, noun: string): string => verb + noun;
 
+// ==============================================
+
 //A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
 
 // Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
@@ -189,16 +191,6 @@
 
 const isPangram = (phrase: string): boolean => {
   phrase = phrase.replace(/[^a-zA-Z]/gi, "");
-  console.log("phrase:", phrase);
   const phraseSet = new Set(phrase.toLowerCase());
-
-  console.log("phraseSet:", phraseSet);
   return phraseSet.size === 26;
 };
-
-console.log(
-  'isPangram("The quick brown fox jumps over the lazy dog."):',
-  isPangram("The quick brown fox jumps over the lazy dog.")
-);
-
-console.log("isPangram('blahblahblah'):", isPangram("blahblahblah"));
